ce7c8b4a60ea943b8e0da3c4b5c00728b4e8a5a6
OumaIntissar <oumaintissar@gmail.com>
2022-03-29 23:10:05 +0000

    First Commit

---
+++ src/main/java/com/capgemini/capfoot/CapfootApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.capfoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapfootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapfootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/capfoot/CapfootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.capfoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapfootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
