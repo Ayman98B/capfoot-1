7f4cb5c05217eedd9622a8abccf88891d9cf73f1
INTISSAR <oumaiyma.intissar@capgemini.com>
2022-04-14 12:51:34 +0000

    Create championat controller

--- src/main/java/com/capgemini/capfoot/CapfootApplication.java
+++ src/main/java/com/capgemini/capfoot/CapfootApplication.java
@@ -1,13 +1,8 @@
 package com.capgemini.capfoot;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.capgemini.capfoot.entity.Championship;
-import com.capgemini.capfoot.repository.ChampionshipRepo;
-import com.capgemini.capfoot.service.ChampionshipService;
 
 @SpringBootApplication
 public class CapfootApplication implements CommandLineRunner {
@@ -19,11 +14,6 @@ public class CapfootApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-
 	}
-
-
-	
-
 
 }
