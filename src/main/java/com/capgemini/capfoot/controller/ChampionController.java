2c876933aa6ad0266300a184cd411d1b3272dd6e
LAFNOUNE <mouad.lafnoune@capgemini.com>
2022-04-14 13:00:53 +0000

    updateChampion

--- src/main/java/com/capgemini/capfoot/controller/ChampionController.java
+++ src/main/java/com/capgemini/capfoot/controller/ChampionController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/championship")
+@RequestMapping("/api/v1/championships")
 public class ChampionController {
 
     @Autowired
