7f9a83ca0040147a08e9f138da97e45085b5f142
LAFNOUNE <mouad.lafnoune@capgemini.com>
2022-04-18 18:02:27 +0000

    adminController

--- src/main/java/com/capgemini/capfoot/controller/AdminController.java
+++ src/main/java/com/capgemini/capfoot/controller/AdminController.java
@@ -3,13 +3,7 @@ package com.capgemini.capfoot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.capgemini.capfoot.entity.Championship;
 import com.capgemini.capfoot.service.ChampionshipService;
@@ -29,6 +23,11 @@ public class AdminController {
 	@PostMapping
 	public void createChampionship(@RequestBody Championship championship) {
 		championshipService.createChampionship(championship);
+	}
+
+	@PutMapping("/updateChampion/{idChampion}")
+	public void updateChampionship(@ @RequestBody Championship championship) {
+		championshipService.updateChampionship(idChampion,championship);
 	}
   
     @DeleteMapping("/{id}")
