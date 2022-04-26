d8aab0426fe74194033f8491b59d111370df49b7
LAFNOUNE <mouad.lafnoune@capgemini.com>
2022-04-26 15:32:49 +0000

    Validation

--- src/main/java/com/capgemini/capfoot/entity/Admin.java
+++ src/main/java/com/capgemini/capfoot/entity/Admin.java
@@ -7,25 +7,37 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+
+
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class Admin {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
+	@NonNull
+	@NotBlank(message = "Votre prénom Obligatoire")
 	private String firstName;
+
+	@NonNull
+	@NotBlank(message = "Votre nom Obligatoire")
 	private String lastName;
+
+	@NotNull
+	@NotBlank(message = "Votre nom Obligatoire")
 	private String password;
+
+	@Email
 	private String emailAdress;
 
 	@OneToMany(mappedBy = "admin")
