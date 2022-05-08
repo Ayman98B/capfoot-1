4a9515f22660dab474c09eb2ab78db1c41aa0026
INTISSAR <oumaiyma.intissar@capgemini.com>
2022-05-08 16:51:03 +0100

    update sites (Casa, Rabat) to enum class

--- src/main/java/com/capgemini/capfoot/CapfootApplication.java
+++ src/main/java/com/capgemini/capfoot/CapfootApplication.java
@@ -1,67 +1,70 @@
 package com.capgemini.capfoot;
 
-import com.capgemini.capfoot.entity.Groupe;
-import com.capgemini.capfoot.entity.Team;
-import com.capgemini.capfoot.repository.GroupRepository;
-import com.capgemini.capfoot.repository.TeamRepository;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.List;
-
+import com.capgemini.capfoot.entity.Site;
+import com.capgemini.capfoot.entity.Team;
+import com.capgemini.capfoot.repository.GroupRepository;
+import com.capgemini.capfoot.repository.TeamRepository;
 
 @SpringBootApplication
 public class CapfootApplication implements CommandLineRunner {
 
-    @Autowired
-    TeamRepository teamRepository;
+	@Autowired
+	TeamRepository teamRepository;
 
-    @Autowired
-    GroupRepository groupRepository;
+	@Autowired
+	GroupRepository groupRepository;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        SpringApplication.run(CapfootApplication.class, args);
-    }
-    
-    List<Team> CASA_TEAMS = Arrays.asList(
-            buildTeamCasa("team_1"), buildTeamCasa("team_2"), buildTeamCasa("team_3"), buildTeamCasa("team_4"),
-            buildTeamCasa("team_5"), buildTeamCasa("team_6"), buildTeamCasa("team_7"), buildTeamCasa("team_8"),
-            buildTeamCasa("team_9"), buildTeamCasa("team_10"), buildTeamCasa("team_11"), buildTeamCasa("team_12"),
-            buildTeamCasa("team_13"), buildTeamCasa("team_14"), buildTeamCasa("team_15"), buildTeamCasa("team_16"));
+		SpringApplication.run(CapfootApplication.class, args);
+	}
 
-    List<Team> RABAT_TEAMS = Arrays.asList(
-            buildTeamRabat("team_r_1"), buildTeamRabat("team_r_2"), buildTeamRabat("team_r_3"), buildTeamRabat("team_r_4"),
-            buildTeamRabat("team_r_5"), buildTeamRabat("team_r_6"), buildTeamRabat("team_r_7"), buildTeamRabat("team_r_8"),
-            buildTeamRabat("team_r_9"), buildTeamRabat("team_r_10"), buildTeamRabat("team_r_11"), buildTeamRabat("team_r_12"),
-            buildTeamRabat("team_r_13"), buildTeamRabat("team_r_14"), buildTeamRabat("team_r_15"), buildTeamRabat("team_r_16"));
+	List<Team> CASA_TEAMS = Arrays.asList(buildTeamCasa("team_1"), buildTeamCasa("team_2"), buildTeamCasa("team_3"),
+			buildTeamCasa("team_4"), buildTeamCasa("team_5"), buildTeamCasa("team_6"), buildTeamCasa("team_7"),
+			buildTeamCasa("team_8"), buildTeamCasa("team_9"), buildTeamCasa("team_10"), buildTeamCasa("team_11"),
+			buildTeamCasa("team_12"), buildTeamCasa("team_13"), buildTeamCasa("team_14"), buildTeamCasa("team_15"),
+			buildTeamCasa("team_16"));
 
-    //List<Groupe> GROUPS = Arrays.asList(buildGroup("A"), buildGroup("B"), buildGroup("C"), buildGroup("D"),
-            //buildGroup("E"), buildGroup("F"), buildGroup("G"), buildGroup("H"));
+	List<Team> RABAT_TEAMS = Arrays.asList(buildTeamRabat("team_r_1"), buildTeamRabat("team_r_2"),
+			buildTeamRabat("team_r_3"), buildTeamRabat("team_r_4"), buildTeamRabat("team_r_5"),
+			buildTeamRabat("team_r_6"), buildTeamRabat("team_r_7"), buildTeamRabat("team_r_8"),
+			buildTeamRabat("team_r_9"), buildTeamRabat("team_r_10"), buildTeamRabat("team_r_11"),
+			buildTeamRabat("team_r_12"), buildTeamRabat("team_r_13"), buildTeamRabat("team_r_14"),
+			buildTeamRabat("team_r_15"), buildTeamRabat("team_r_16"));
 
-    private static Team buildTeamCasa(String teamName) {
+	// List<Groupe> GROUPS = Arrays.asList(buildGroup("A"), buildGroup("B"),
+	// buildGroup("C"), buildGroup("D"),
+	// buildGroup("E"), buildGroup("F"), buildGroup("G"), buildGroup("H"));
 
-        return new Team(teamName, "Casa");
-    }
+	private static Team buildTeamCasa(String teamName) {
 
-    private static Team buildTeamRabat(String teamName) {
+		return new Team(teamName, Site.CASABLANCA);
+	}
 
-        return new Team(teamName, "RABAT");
-    }
+	private static Team buildTeamRabat(String teamName) {
 
-    //private static Groupe buildGroup(String groupName) {
+		return new Team(teamName, Site.RABAT);
+	}
 
-        //return new Groupe(groupName);
-    //}
+	// private static Groupe buildGroup(String groupName) {
 
-    @Override
-    public void run(String... args) {
-        teamRepository.saveAll(CASA_TEAMS);
-        teamRepository.saveAll(RABAT_TEAMS);
-        //groupRepository.saveAll(GROUPS);
-    }
+	// return new Groupe(groupName);
+	// }
+
+	@Override
+	public void run(String... args) {
+
+		teamRepository.saveAll(CASA_TEAMS);
+		teamRepository.saveAll(RABAT_TEAMS);
+		// groupRepository.saveAll(GROUPS);
+	}
 
 }
