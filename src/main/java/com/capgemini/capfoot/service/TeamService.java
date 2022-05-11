83a59995e378f6942421f3d1b6ffb2ba12a2c3db
INTISSAR <oumaiyma.intissar@capgemini.com>
2022-05-12 00:01:34 +0100

    Add getAllTeamsByChampionat to teamService

--- src/main/java/com/capgemini/capfoot/service/TeamService.java
+++ src/main/java/com/capgemini/capfoot/service/TeamService.java
@@ -6,13 +6,20 @@ import java.util.List;
 
 public interface TeamService {
 
-     Team addTeam(Team team);
-     Team getTeamByName(String name);
-     List<Team> gatAllTeam();
-     Team updateTeam (Team team);
-     Boolean deleteTeam(Team team);
-     Boolean deleteTeamById(Long id);
-     Team inscription(Team team);
+	Team addTeam(Team team);
 
+	Team getTeamByName(String name);
+
+	List<Team> gatAllTeam();
+
+	Team updateTeam(Team team);
+
+	Boolean deleteTeam(Team team);
+
+	Boolean deleteTeamById(Long id);
+
+	Team inscription(Team team);
+
+	List<Team> getAllTeamsByChampionat(Long idChamp);
 
 }
