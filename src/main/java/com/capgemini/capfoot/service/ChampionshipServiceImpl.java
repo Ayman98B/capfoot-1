a18ca605790f55dd4f980ad9938fb02f447d3375
ALLABOU <anass.allabou@capgemini.com>
2022-05-13 03:36:30 +0100

    championship service impl

--- src/main/java/com/capgemini/capfoot/service/ChampionshipServiceImpl.java
+++ src/main/java/com/capgemini/capfoot/service/ChampionshipServiceImpl.java
@@ -95,9 +95,9 @@ public class ChampionshipServiceImpl implements ChampionshipService {
 			if (oldChamp.getStatut() != updateChamp.getStatut()) {
 
 				List<Team> teams = teamService.getAllTeamsByChampionat(oldChamp.getId());
-				List<MatchDisputee> allMatchs = matchService.getAllMatchs();
-				log.info("updating match stage");
-				allMatchs.forEach((matchDisputee -> matchDisputee.setStage(updateChamp.getStatut())));
+				//List<MatchDisputee> allMatchs = matchService.getAllMatchs();
+				//log.info("updating match stage");
+				//allMatchs.forEach((matchDisputee -> matchDisputee.setStage(updateChamp.getStatut())));
         
 				log.info("Sending Email ...");
 				sendEmail(oldChamp);
